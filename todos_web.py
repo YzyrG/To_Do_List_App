26eafd82fcd0c88db6929ccf25939d0001220b9e
Zyr <zyr724725@gmail.com>
2023-03-02 20:10:25 +0800

    create add function

--- todos_web.py
+++ todos_web.py
@@ -5,8 +5,15 @@ To_Do_List Web style
 import streamlit as st  # streamlit 是开源的web前端框架
 import todos_functions
 
-
 todos = todos_functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'  # st.session_state是字典，'new_todo'是它的一个key
+    todos.append(todo)
+    todos_functions.put_todos(todos)
+    # print(todo)
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app!")
@@ -16,4 +23,5 @@ for todo in todos:
     todo = todo.strip('\n')
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter a new todo.")
+st.text_input(label="", placeholder="Enter a new todo.",
+              on_change=add_todo, key="new_todo")
